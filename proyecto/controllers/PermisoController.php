97b43c5a140f47e9176d034af75562b9f354988b
Victor Raúl García García <victor07mejor@gmail.com>
2026-05-16 16:22:34 -0600

    fix: corrige erroes

--- proyecto/controllers/PermisoController.php
+++ proyecto/controllers/PermisoController.php
@@ -17,8 +17,8 @@ class PermisoController extends ActiveController
         $behaviors['corsFilter'] = [
             'class' => \yii\filters\Cors::class,
             'cors' => [
-                'Origin'                           => ['http://localhost:8100', 'http://localhost:8101', '*'], // Accept from local ionic
-                'Access-Control-Request-Method'    => ['GET'],
+                'Origin'                           => ['http://localhost:8100', 'http://localhost:8101'], // Accept from local ionic
+                'Access-Control-Request-Method'    => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
                 'Access-Control-Request-Headers'   => ['*'],
                 'Access-Control-Allow-Credentials' => true,
                 'Access-Control-Max-Age'           => 600
@@ -28,7 +28,8 @@ class PermisoController extends ActiveController
             'class' => CompositeAuth::class,
             'authMethods' => [
                 HttpBearerAuth::class,
-            ]
+            ],
+            'except' => ['options']
         ];
         return $behaviors;
     }
